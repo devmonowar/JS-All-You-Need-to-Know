4592701e6189ba926389269a224e655866a815f4
kstmonowar <kstmonowar@gmail.com>
2023-03-21 00:39:57 +0600

    nestedloop

--- Chapter_four/nestedloop.js
+++ Chapter_four/nestedloop.js
@@ -11,7 +11,7 @@
 let n = 5
 for (let i = 1; i <= n; i++) {
     let result = ''
-    for (let j = 1; j <= n; j++) {
+    for (let j = 1; j <= ; j++) {
         result += '* '        
     }
     console.log(result)
