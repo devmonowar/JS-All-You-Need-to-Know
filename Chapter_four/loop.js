01feda69d855c6e3d1965324491bb5111d2eccec
kstmonowar <kstmonowar@gmail.com>
2023-03-21 00:37:39 +0600

    nestedloop

--- Chapter_four/loop.js
+++ Chapter_four/loop.js
@@ -11,14 +11,29 @@
 
 
 
-var sum = 0;
+// var sum = 0;
 
-for (var i = 1; i <= 10; i++) {
+// for (var i = 1; i <= 10; i++) {
     
-    console.log(sum + '+' + i + '=' + (sum + i ));
+//     console.log(sum + '+' + i + '=' + (sum + i ));
 
-    sum += i
+//     sum += i
     
-}
+// }
+
+// console.log('result =' + sum)
 
 
+    var isRunning = true
+
+
+    while (isRunning) {
+        var rand = Math.floor(Math.random() * 10 + 1)
+        if (rand == 9 ) {
+            console.log('Winner Winner Chicken Dinner')
+            isRunning = false
+        } else {
+            console.log('You have got ' + rand)
+        }
+    }
+    
---
+++ Chapter_four/nestedloop.js
@@ -0,0 +1,18 @@
+// let n = 10
+// for (let i = 1; i <= n; i++) {
+//     let result = ''
+//     for (let j = 1; j <= i; j++) {
+//         result += j +' '
+//     }
+//     console.log(result)
+// }
+
+
+let n = 5
+for (let i = 1; i <= n; i++) {
+    let result = ''
+    for (let j = 1; j <= n; j++) {
+        result += '* '        
+    }
+    console.log(result)
+}
