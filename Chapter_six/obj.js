db4abef5fab674f6e2fe2f8b582db95584596af7
kstmonowar <kstmonowar@gmail.com>
2023-03-24 00:36:16 +0600

    obj

--- Chapter_six/obj.js
+++ Chapter_six/obj.js
@@ -4,7 +4,24 @@ var obj = {
     z: 75
 }
 
-for( var i in obj){
+// for( var i in obj){
     
-    console.log(i + ' is : ' + obj[i])
-}
+//     console.log(i + ' is : ' + obj[i])
+// }
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+// console.log(Object.entries(obj))
+
+// var obj2 = obj
+
+// obj2.x = 50
+
+// console.log(obj2)
+
+
+var obj2 = Object.assign({}, obj)
+obj2.x = 70
+
+console.log(obj)
+console.log(obj2)
