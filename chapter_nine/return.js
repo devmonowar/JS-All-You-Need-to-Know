7e3c5a01fd3853c4b4d8f13472cb8b224ee6a413
kstmonowar <kstmonowar@gmail.com>
2023-03-26 16:33:10 +0600

    basic

--- chapter_nine/return.js
+++ chapter_nine/return.js
@@ -1,14 +1,30 @@
-function greet(msg) {
-    function greetings(name) {
-        return msg + ', ' + name + '!'
+// function greet(msg) {
+//     function greetings(name) {
+//         return msg + ', ' + name + '!'
+//     }
+
+//     return greetings
+// }
+// var gm = greet('Good Morning')
+// var gn = greet('Good Night')
+// var hello = greet('Hello bro')
+// // console.log(typeof gm)
+
+// var msg = gm('Monowar Hossain')
+
+// console.log(msg)
+// console.log(gn('Monowar Hossain'))
+// console.log(hello('Monowar Hossain'))
+
+function base(b) {
+    return function (n) {
+        var result = 1
+        for (let i = 0; i < b; i++) {
+            result *= n 
+        }
+        return result
     }
-
-    return greetings
 }
-var gm = greet('Good Morning')
-// console.log(typeof gm)
 
-var msg = gm('Monowar Hossain')
-
-console.log(msg)
-
+var base10 = base(10)
+console.log(base10(2))
