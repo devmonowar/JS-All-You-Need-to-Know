a3fba4f85a4811439abd394da1b3c12864deb79b
kstmonowar <kstmonowar@gmail.com>
2023-03-26 12:44:08 +0600

    find js file add

---
+++ chapter_nine/find.js
@@ -0,0 +1,13 @@
+var arr = [2, 5, 6, 3, 9, 6, 7, 78, 45]
+
+var result = arr.find(function(value){
+    return value == 9
+})
+
+console.log(result)
+
+// var result = arr.findIndex(function(value){
+//     return value == 9
+// })
+
+// console.log(result)
