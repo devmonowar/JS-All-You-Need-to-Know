63002485eb9e458b7b73e585b15a4988b5bdbe80
kstmonowar <kstmonowar@gmail.com>
2023-03-26 13:04:24 +0600

    myFind

--- chapter_nine/find.js
+++ chapter_nine/find.js
@@ -1,13 +1,28 @@
-var arr = [2, 5, 6, 3, 9, 6, 7, 78, 45]
+ var arr = [2, 5, 6, 3, 9, 6, 7, 78, 45]
 
-var result = arr.find(function(value){
-    return value == 9
-})
+// var result = arr.find(function(value){
+//     return value == 9
+// })
 
-console.log(result)
+// console.log(result)
 
 // var result = arr.findIndex(function(value){
 //     return value == 9
 // })
 
 // console.log(result)
+
+function myFind(arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+       
+        if (cb(arr[i])) {
+             return arr[i]
+           //  return i
+        }        
+    }    
+}
+var result = myFind(arr, function (value) {
+    return value == 9
+})
+
+console.log(result)
