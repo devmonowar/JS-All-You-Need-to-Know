ac9132e853c5d638361b86c3ab20788327da67c2
kstmonowar <kstmonowar@gmail.com>
2023-03-26 16:40:37 +0600

    power

--- chapter_nine/return.js
+++ chapter_nine/return.js
@@ -28,3 +28,9 @@ function base(b) {
 
 var base10 = base(10)
 console.log(base10(2))
+
+var base5 = base(5)
+console.log(base5(2))
+console.log(base5(3))
+console.log(base5(5))
+console.log(base5(10))
