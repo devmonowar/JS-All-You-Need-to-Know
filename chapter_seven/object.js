57e053d54da582d942896291de5a75ecb2377687
kstmonowar <kstmonowar@gmail.com>
2023-03-27 01:26:15 +0600

    object add replace

--- chapter_seven/object.js
+++ chapter_seven/object.js
@@ -2,11 +2,50 @@
 // obj.x = 10
 // console.log(obj)
 
+// var point = {
+//     x: 10,
+//     y: 20
+// }
+
+// // point.y = 30
+// point.z = 30
+
+// console.log(point)
+
+
+// var obj = Object()
+// obj.a = 10
+// console.log(obj)
+
+// var obj2 = new Object()
+// obj2.b = 20
+// console.log(obj2)
+
+
 var point = {
     x: 10,
-    y: 20
+    y: 20,
+    z: 15
 }
 
-point.z = 39
+// console.log(point.x)
+// console.log(point.y)
+// console.log(point.x + point.z)
 
+// console.log(point['x'])
+// console.log(point['z'])
+
+// var show = 'x'
+// console.log(point[show])
+
+point.x = 45
+point.a = 16
+
+console.log(point)
+console.log(point.d)
+
+point['y'] = 100
+
+var prop = 'z'
+point[prop] = 55
 console.log(point)
