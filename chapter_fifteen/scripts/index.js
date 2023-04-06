4e72df432f58d67420793db496c7fb124362c14d
kstmonowar <kstmonowar@gmail.com>
2023-04-06 17:22:38 +0600

    test

---
+++ chapter_fifteen/scripts/index.js
@@ -0,0 +1,51 @@
+// import '../styles/index.scss'
+
+// import { sum, sub  } from './math'
+
+// console.log('Hello Webpack 4')
+
+// console.log(sum(10, 20))
+// console.log(sub(10, 20))
+
+// alert('Hello Catians')
+
+// var age = 16
+// var fname = 'Monowar Hossain'
+
+// console.log(`My name is ${fname} and I am ${age} years old. I am ${age < 18 ? 'not ' : ''}adult`)
+
+// console.log(fname.padStart(25, '*'))
+// console.log(fname.padEnd(25, 'a'))
+// console.log('S'.repeat(10))
+
+
+// function add(a, b) {
+//     return a+b
+// }
+
+// let add = x => {
+//     return a + b
+// }
+
+// console.log(add(4, 5))
+
+// let add = x => x*x;
+// console.log(add(5))
+console.log('test')
+
+
+
+let obj = {
+    fname: 'Monowar Hossain',
+    print: function(){
+        setTimeout(() => {
+            console.log(this)
+            console.log(`Hello, ${this.fname}`)
+        }, 1000)
+    }
+}
+obj.print()
+
+
+
+
